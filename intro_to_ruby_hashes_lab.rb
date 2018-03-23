7f10a0186bd6444bd2352f24227c87186d468e34
Ryan Hu <ryanhuuu@gmail.com>
2018-03-23 19:26:56 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4, 
+monopoly = {railroads: {pieces: 4, 
 	                        names: {},
 	                        rent_in_dollars: {}
 	                        }, 
@@ -32,7 +31,6 @@ def monopoly_with_second_tier
 	            rent_in_dollars: {}
 	            }
 	
-end
 end
 
 def monopoly_with_fourth_tier
